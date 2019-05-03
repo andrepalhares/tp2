bf5758471d1aa875ca7aab45025961ca8064f5ee
andrepalhares <33261635+andrepalhares@users.noreply.github.com>
2019-05-03 13:28:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <stdbool.h>
 
 #define earthRadiusKm 6371.0
@@ -27,16 +26,16 @@ int distanceEarthKm(double lat1d, double lon1d, double lat2d, double lon2d) {
   return (int) 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
 }
 
-int qte_vertices(char nomearquivo[64]) {
-	FILE *cidades;
-	cidades = fopen(nomearquivo, "r");
+int qte_vertices(char nome[64]) {
+  FILE *cidades;
+	cidades = fopen(nome, "r");
+  int qte_cidades;
 
-	char qte_cidades = fgetc(cidades);
-	int num_cidades = atoi(&qte_cidades);
+	fscanf (cidades, "%d", &qte_cidades);
 
 	fclose(cidades);
 
-	return num_cidades;
+	return qte_cidades;
 }
 
 void inicia_pontos(char nome[64], int qte_vertices, struct Ponto cidades[]) {
@@ -54,10 +53,11 @@ void inicia_pontos(char nome[64], int qte_vertices, struct Ponto cidades[]) {
   }
 }
 
-void inicia_arestas(char nome[64], int qte_vertices, struct Ponto cidades[], int arestas[][qte_vertices]) {
+void inicia_arestas(char nome[64], int qte_vertices, struct Ponto cidades[], int arestas[][qte_vertices], int *max_aresta) {
+  int aux_max = 0;
   //Caso 1: A diagonal da matriz é sempre 0, pois é a distância do ponto a ele mesmo
   for (int i = 0; i < qte_vertices; i++) {
-    arestas[i][i] = 0000;
+    arestas[i][i] = 0;
   }
 
   //Caso 2: agrupamento de uma aresta i com todos os vertices i+1 até que i seja igual a qte_vertices
@@ -65,17 +65,25 @@ void inicia_arestas(char nome[64], int qte_vertices, struct Ponto cidades[], int
     for (int j=i+1; j < qte_vertices; j++) {
       arestas[i][j] = distanceEarthKm(cidades[i].x, cidades[i].y, cidades[j].x, cidades[j].y);
       arestas[j][i] = arestas[i][j];
+
+      if(aux_max < arestas[i][j]) {
+        aux_max = arestas[i][j];
+      }
+
     }
   }
+
+  *max_aresta = aux_max;
 
 }
 
 // A utility function to find the vertex with
 // minimum key value, from the set of vertices
 // not yet included in MST
-int minKey(int key[], bool mstSet[], int qte_ver) {
+int minKey(int key[], bool mstSet[], int qte_ver, int max_aresta) {
 	// Initialize min value
-	int min = INT_MAX, min_index;
+	int min = max_aresta + 1;
+  int min_index;
 
 	for (int v = 0; v < qte_ver; v++)
 		if (mstSet[v] == false && key[v] < min)
@@ -87,15 +95,19 @@ int minKey(int key[], bool mstSet[], int qte_ver) {
 // A utility function to print the
 // constructed MST stored in parent[]
 int printMST(int parent[], int qte_ver, int graph[qte_ver][qte_ver]) {
-	printf("Edge \tWeight\n");
-	for (int i = 1; i < qte_ver; i++)
-		printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+  int max = 0;
+	for (int i = 1; i < qte_ver; i++) {
+    if(max < graph[i][parent[i]]) {
+      max = graph[i][parent[i]];
+    }
+  }
+  printf("%d", max);
 }
 
 // Function to construct and print MST for
 // a graph represented using adjacency
 // matrix representation
-void primMST(int qte_ver, int graph[qte_ver][qte_ver]) {
+void primMST(int qte_ver, int graph[qte_ver][qte_ver], int max_aresta) {
 	// Array to store constructed MST
 	int parent[qte_ver];
 	// Key values used to pick minimum weight edge in cut
@@ -104,8 +116,10 @@ void primMST(int qte_ver, int graph[qte_ver][qte_ver]) {
 	bool mstSet[qte_ver];
 
 	// Initialize all keys as INFINITE
-	for (int i = 0; i < qte_ver; i++)
-		key[i] = INT_MAX, mstSet[i] = false;
+	for (int i = 0; i < qte_ver; i++) {
+		key[i] = max_aresta + 1;
+    mstSet[i] = false;
+  }
 
 	// Always include first 1st vertex in MST.
 	// Make key 0 so that this vertex is picked as first vertex.
@@ -117,7 +131,7 @@ void primMST(int qte_ver, int graph[qte_ver][qte_ver]) {
 	{
 		// Pick the minimum key vertex from the
 		// set of vertices not yet included in MST
-		int u = minKey(key, mstSet, qte_ver);
+		int u = minKey(key, mstSet, qte_ver, max_aresta);
 
 		// Add the picked vertex to the MST Set
 		mstSet[u] = true;
@@ -140,20 +154,14 @@ void primMST(int qte_ver, int graph[qte_ver][qte_ver]) {
 }
 
 void main() {
-  int qte_ver = qte_vertices("cidades1.txt");
+  int max_aresta;
+  int qte_ver = qte_vertices("cidades2.txt");
   struct Ponto cidades[qte_ver];
   int arestas[qte_ver][qte_ver];
 
-  inicia_pontos("cidades1.txt", qte_ver, cidades);
-  inicia_arestas("cidades1.txt", qte_ver, cidades, arestas);
+  inicia_pontos("cidades2.txt", qte_ver, cidades);
+  inicia_arestas("cidades2.txt", qte_ver, cidades, arestas, &max_aresta);
 
-  primMST(qte_ver, arestas);
-
-  /*for (int i=0; i < qte_ver; i++) {
-    for(int j=0; j < qte_ver; j++) {
-      printf("%d ", arestas[i][j]);
-    }
-    printf("\n");
-  }*/
-
+  primMST(qte_ver, arestas, max_aresta);
+  
 }
