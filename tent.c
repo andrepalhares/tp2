781611ef7d5321d6452123e1b7c4a34ae0278015
andrepalhares <33261635+andrepalhares@users.noreply.github.com>
2019-05-16 11:46:10 -0300

    funcionando para 31, 31, 31, 31...

--- tent.c
+++ tent.c
@@ -51,28 +51,33 @@ void partition(int arr[], int n, int pivot) {
 
   int pos_pivo = 0;
   int pos_final = n - 1;
+  int aux_pivo = 0;
 
   for(int k=1; k < n; k++) {
-    if (arr[pos_pivo] > arr[pos_pivo + 1]) {
-      troca_elemento(&arr[pos_pivo], &arr[pos_pivo + 1]);
+    if (arr[aux_pivo] > arr[aux_pivo + 1]) {
+      troca_elemento(&arr[pos_pivo], &arr[aux_pivo + 1]);
       pos_pivo++;
-    } else if (arr[pos_pivo] < arr[pos_pivo + 1]) {
-      troca_elemento(&arr[pos_pivo + 1], &arr[pos_final]);
+      aux_pivo++;
+    } else if (arr[aux_pivo] < arr[aux_pivo + 1]) {
+      troca_elemento(&arr[aux_pivo + 1], &arr[pos_final]);
       pos_final--;
     } else {
-      pos_pivo++;
+      aux_pivo++;
     }
     //printf("pivo %d pos pivo %d pos final %d\n", arr[pos_pivo], pos_pivo, pos_final);
   }
 
-  for(int a = 0; a < n; a++) {
+  printf("pos final pivo: %d\npos final ultimo pivo: %d\npos que queremos: %d\n", pos_pivo, aux_pivo, n/2);
+
+  /*for(int a = 0; a < n; a++) {
     printf("%d ", arr[a]);
-  }
+  }*/
 
 }
 
 void main() {
   int arr[54] = {29,22,28,14,45,10,44,23,9,39,38,52,6,5,50,37,11,26,3,15,2,53,40,54,25,55,12,19,30,16,18,13,1,48,41,24,43,46,47,17,34,20,31,32,22,35,4,49,51,7,36,27,8,21};
+  qsort(arr, 54, sizeof(int), compare);
   int n = 54;
   int pivot = mom(arr, n);
   partition(arr, n, pivot);
