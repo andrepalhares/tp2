3b9c168b5c4a94e6c46565b34656e386e36b040c
andrepalhares <33261635+andrepalhares@users.noreply.github.com>
2019-05-18 09:17:04 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,94 +1,61 @@
-// A C Program to demonstrate adjacency list
-// representation of graphs
 #include <stdio.h>
 #include <stdlib.h>
 
-// A structure to represent an adjacency list node
-struct AdjListNode
-{
-    int dest;
-    int weight;
-    struct AdjListNode* next;
+struct Vertice {
+    int seguinte;
+    int peso;
+    struct Vertice* proximo;
 };
 
-// A structure to represent an adjacency list
-struct AdjList
-{
-    struct AdjListNode *head;
+struct Lista {
+    struct Vertice *cabeca;
 };
 
-// A structure to represent a graph. A graph
-// is an array of adjacency lists.
-// Size of array will be V (number of vertices
-// in graph)
-struct Graph
-{
-    int V;
-    struct AdjList* array;
+struct Grafo {
+    int qte_vertices;
+    struct Lista* sequencia;
 };
 
-// A utility function to create a new adjacency list node
-struct AdjListNode* newAdjListNode(int dest, int weight)
-{
-    struct AdjListNode* newNode =
-     (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
-    newNode->dest = dest;
-    newNode->weight = weight;
-    newNode->next = NULL;
-    return newNode;
+struct Vertice* adiciona_vertice(int seguinte, int distancia) {
+    struct Vertice* no_criado = (struct Vertice*) malloc(sizeof(struct Vertice));
+    no_criado->seguinte = seguinte;
+    no_criado->peso = distancia;
+    no_criado->proximo = NULL;
+    return no_criado;
 }
 
-// A utility function that creates a graph of V vertices
-struct Graph* createGraph(int V)
-{
-    struct Graph* graph =
-        (struct Graph*) malloc(sizeof(struct Graph));
-    graph->V = V;
+struct Grafo* cria_grafo(int qte_vertices) {
+    struct Grafo* grafo_criado = (struct Grafo*) malloc(sizeof(struct Grafo));
+    grafo_criado->qte_vertices = qte_vertices;
+    grafo_criado->sequencia = (struct Lista*) malloc(qte_vertices * sizeof(struct Lista));
 
-    // Create an array of adjacency lists.  Size of
-    // array will be V
-    graph->array =
-      (struct AdjList*) malloc(V * sizeof(struct AdjList));
+    for (int i = 0; i < qte_vertices; ++i) {
+        grafo_criado->sequencia[i].cabeca = NULL;
+    }
 
-    // Initialize each adjacency list as empty by
-    // making head as NULL
-    int i;
-    for (i = 0; i < V; ++i)
-        graph->array[i].head = NULL;
-
-    return graph;
+    return grafo_criado;
 }
 
-// Adds an edge to an undirected graph
-void addEdge(struct Graph* graph, int src, int dest, int weight)
-{
-    // Add an edge from src to dest.  A new node is
-    // added to the adjacency list of src.  The node
-    // is added at the begining
-    struct AdjListNode* newNode = newAdjListNode(dest, weight);
-    newNode->next = graph->array[src].head;
-    graph->array[src].head = newNode;
+void adiciona_aresta(struct Grafo* grafo, int a, int b, int distancia) {
+    struct Vertice* no_criado = adiciona_vertice(a, distancia);
+    no_criado->proximo = grafo->sequencia[b].cabeca;
+    grafo->sequencia[b].cabeca = no_criado;
 
-    // Since graph is undirected, add an edge from
-    // dest to src also
-    newNode = newAdjListNode(src, weight);
-    newNode->next = graph->array[dest].head;
-    graph->array[dest].head = newNode;
+    no_criado = adiciona_vertice(b, distancia);
+    no_criado->proximo = grafo->sequencia[a].cabeca;
+    grafo->sequencia[a].cabeca = no_criado;
 }
 
-// A utility function to print the adjacency list
-// representation of graph
-void printGraph(struct Graph* graph)
+void imprime(struct Grafo* grafo)
 {
-    int v;
-    for (v = 0; v < graph->V; ++v)
+    for (int v = 0; v < grafo->qte_vertices; v++)
     {
-        struct AdjListNode* pCrawl = graph->array[v].head;
+        struct Vertice* no_aux = grafo->sequencia[v].cabeca;
         printf("%d: ", v);
-        while (pCrawl)
+        while (no_aux)
         {
-            printf("-> %d (%d) ", pCrawl->dest, pCrawl->weight);
-            pCrawl = pCrawl->next;
+            printf("-> %d (%d) ", no_aux->seguinte, no_aux->peso);
+            no_aux = no_aux->proximo;
         }
         printf("\n");
     }
@@ -99,17 +66,17 @@ int main()
 {
     // create the graph given in above fugure
     int V = 5;
-    struct Graph* graph = createGraph(V);
-    addEdge(graph, 0, 1, 5);
-    addEdge(graph, 0, 4, 7);
-    addEdge(graph, 1, 2, 6);
-    addEdge(graph, 1, 3, 11);
-    addEdge(graph, 1, 4, 99);
-    addEdge(graph, 2, 3, 52);
-    addEdge(graph, 3, 4, 75);
+    struct Grafo* graph = cria_grafo(V);
+    adiciona_aresta(graph, 0, 1, 5);
+    adiciona_aresta(graph, 0, 4, 7);
+    adiciona_aresta(graph, 1, 2, 6);
+    adiciona_aresta(graph, 1, 3, 11);
+    adiciona_aresta(graph, 1, 4, 99);
+    adiciona_aresta(graph, 2, 3, 52);
+    adiciona_aresta(graph, 3, 4, 75);
 
     // print the adjacency list representation of the above graph
-    printGraph(graph);
+    imprime(graph);
 
     return 0;
 }
